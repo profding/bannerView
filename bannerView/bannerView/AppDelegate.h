9b1199a088b549a7bb6bc9c9ca4564f541214644
profding <1647262710@qq.com>
2016-03-22 10:49:39 +0800

    创建bannerView工程

---
+++ bannerView/bannerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bannerView
+//
+//  Created by 丁贵强 on 16/3/22.
+//  Copyright © 2016年 丁贵强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
